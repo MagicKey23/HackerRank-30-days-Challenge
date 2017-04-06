6aa428bf5e3ea5f3da11b26d577d597f409ed871
Anh Nguyen <schwannguyen@gmail.com>
2017-04-05 20:45:42 -0400

    Create day10Recursion.java

---
+++ day10Recursion.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+        int Kappa = in.nextInt();
+        int result = factorial(Kappa);
+        System.out.println(result);
+        
+       
+    }
+    public static int factorial(int n){
+        if (n <= 1 ){
+            return n;
+        }else{return n * factorial(n-1);}
+    }
+}
