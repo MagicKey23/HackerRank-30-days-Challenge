56319702ace032d93db331da7385dc2584e64531
Anh Nguyen <schwannguyen@gmail.com>
2017-04-05 20:34:14 -0400

    Create day1Datatypes.java

---
+++ day1Datatypes.java
@@ -0,0 +1,22 @@
+  /* Declare second integer, double, and String variables. */
+        int n;
+        double d2;
+        String s2;
+        /* Read and save an integer, double, and String to your variables.*/
+        n = scan.nextInt();
+        System.out.println(i + n);
+        d2 = scan.nextDouble(); 
+         System.out.println(d + d2);
+        scan.nextLine();
+        s2 = scan.nextLine();
+        System.out.println(s + s2);
+
+        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+
+        /* Print the sum of both integer variables on a new line. */
+    
+        /* Print the sum of the double variables on a new line. */
+		  
+        /* Concatenate and print the String variables on a new line; 
+        	the 's' variable above should be printed first. */
+     
